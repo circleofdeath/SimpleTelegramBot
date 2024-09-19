d30fa2a874074e3531246ae103f3d473ceb2f9d4
circleofdeath <deltaalphaengi@proton.me>
2024-09-19 15:38:12 +0300

    bot

---
+++ src/dae/telegrambothomework/Bot.java
@@ -0,0 +1,48 @@
+package dae.telegrambothomework;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@SuppressWarnings("ALL") // send message is deprecated
+public class Bot extends TelegramLongPollingBot {
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new Bot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            Long chatId = update.getMessage().getChatId();
+
+            SendMessage message = new SendMessage();
+            message.setChatId(chatId);
+            message.setText("You said: " + messageText);
+
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "BillCipherBallsBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "8166957450:AAGOC4x0ji7QeDHyDUbLHFhvaj9GH1yoDhs";
+    }
+}
